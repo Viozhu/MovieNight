8100b3bb13b0943037912834937cc36722ac2d84
Viozhu <nachog_man@icloud.com>
2023-02-19 07:18:18 +0900

    feat: proto navbar

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { RouterProvider } from "react-router-dom";
+import { NavBar } from "./Components";
 import "./index.css";
 import { router } from "./routes";
 
@@ -9,6 +10,7 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
+    <NavBar />
     <RouterProvider router={router} />
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
