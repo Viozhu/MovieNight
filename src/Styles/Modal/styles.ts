29364fce1bd2e88d56d247b7fe32287bbb048e06
Viozhu <nachog_man@icloud.com>
2023-02-22 04:22:27 +0900

    fix: styles

--- src/Styles/Modal/styles.ts
+++ src/Styles/Modal/styles.ts
@@ -2,10 +2,10 @@ export const MODAL_CONTAINER =
   'flex fixed bg-modal z-50 w-screen h-screen items-center justify-center left-0 top-0 right-0 bottom-0 ';
 
 export const INNER_CONTENT =
-  'flex items-center justify-center bg-transparent md:p-4  h-auto  mt-12 md:mt-0 md:h-full';
+  'flex items-center justify-center bg-transparent md:p-4  h-auto  my-auto md:mt-0 md:h-full';
 
 export const MODAL_CONTENT =
-  'flex flex-col self-center p-4 pt-32 h-auto md:p-5 md:px-6 items-center bg-white rounded border-sm relative w-full  ';
+  'flex flex-col self-center p-4  h-auto md:p-5 md:px-6 items-center bg-white rounded border-sm relative w-full  ';
 
 export const TITLE_CONTAINER =
   'flex w-full items-center select-none justify-center';
